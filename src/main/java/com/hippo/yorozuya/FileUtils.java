640d412a7ee4d48a5fa64e0c657cb48f564f1594
seven332 <seven332@163.com>
2015-07-14 15:19:11 +0800

    Add convert byte to human readable string

--- src/main/java/com/hippo/yorozuya/FileUtils.java
+++ src/main/java/com/hippo/yorozuya/FileUtils.java
@@ -35,4 +35,20 @@ public final class FileUtils {
             return false;
         }
     }
+
+    /**
+     * Convert byte to human readable string.<br/>
+     * http://stackoverflow.com/questions/3758606/
+     *
+     * @param bytes the bytes to convert
+     * @param si si units
+     * @return the human readable string
+     */
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
 }
