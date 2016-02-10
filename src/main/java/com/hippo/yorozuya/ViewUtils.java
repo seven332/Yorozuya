fe276a68f183cb3639fdd77a0f46e5a5b6fafccd
seven332 <seven332@163.com>
2016-02-11 00:19:46 +0800

    Add ViewUtils.generateViewId()

--- src/main/java/com/hippo/yorozuya/ViewUtils.java
+++ src/main/java/com/hippo/yorozuya/ViewUtils.java
@@ -27,10 +27,13 @@ import android.view.ViewParent;
 import android.view.ViewTreeObserver;
 
 import java.io.PrintWriter;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class ViewUtils {
 
     public static final int MAX_SIZE = Integer.MAX_VALUE & ~(0x3 << 30);
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
 
     /**
      * Get view center location in window
@@ -421,5 +424,23 @@ public final class ViewUtils {
             }
         }
         writer.flush();
+    }
+
+    /**
+     * Generate a value suitable for use in {@link View#setId(int)}.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     *
+     * @return a generated ID value
+     */
+    public static int generateViewId() {
+        for (;;) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
     }
 }
