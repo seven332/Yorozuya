431bae2665cacfdea83f66900b5c4cd5283a585c
seven332 <seven332@163.com>
2016-05-10 00:26:10 +0800

    Add memory method getAppAllocatedMemory, getAppMaxMemory, getTotalMemory

--- library/src/main/java/com/hippo/yorozuya/OSUtils.java
+++ library/src/main/java/com/hippo/yorozuya/OSUtils.java
@@ -18,12 +18,71 @@ package com.hippo.yorozuya;
 
 import android.os.Looper;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class OSUtils {
     private OSUtils() {}
+
+    private static final String PROCFS_MEMFILE = "/proc/meminfo";
+    private static final Pattern PROCFS_MEMFILE_FORMAT =
+            Pattern.compile("^([a-zA-Z]*):[ \t]*([0-9]*)[ \t]kB");
+    private static final String MEMTOTAL_STRING = "MemTotal";
+
+    private static long sTotalMem = Long.MIN_VALUE;
 
     public static void checkMainLoop() {
         if (Looper.myLooper() != Looper.getMainLooper()) {
             throw new IllegalStateException("It is in not main loop!");
         }
+    }
+
+    /**
+     * Get application allocated memory size
+     */
+    public static long getAppAllocatedMemory() {
+        return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+    }
+
+    /**
+     * Get application max memory size
+     */
+    public static long getAppMaxMemory() {
+        return Runtime.getRuntime().maxMemory();
+    }
+
+    /**
+     * Get device RAM size
+     */
+    public static long getTotalMemory() {
+        if (sTotalMem == Long.MIN_VALUE) {
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new FileReader(PROCFS_MEMFILE), 64);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Matcher matcher = PROCFS_MEMFILE_FORMAT.matcher(line);
+                    if (matcher.find() && MEMTOTAL_STRING.equals(matcher.group(1))) {
+                        long mem = NumberUtils.parseLongSafely(matcher.group(2), -1L);
+                        if (mem != -1L) {
+                            mem *= 1024;
+                        }
+                        sTotalMem = mem;
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                IOUtils.closeQuietly(reader);
+            }
+            if (sTotalMem == Long.MIN_VALUE) {
+                sTotalMem = -1L;
+            }
+        }
+        return sTotalMem;
     }
 }
