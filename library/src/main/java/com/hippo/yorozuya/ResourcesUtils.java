9f47861daa71205969c4cd74fe743f201b634457
seven332 <seven332@163.com>
2016-08-01 18:45:02 +0800

    Add more get attr in ResourcesUtils

--- library/src/main/java/com/hippo/yorozuya/ResourcesUtils.java
+++ library/src/main/java/com/hippo/yorozuya/ResourcesUtils.java
@@ -19,75 +19,171 @@ package com.hippo.yorozuya;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.AttrRes;
+import android.support.annotation.CheckResult;
+import android.support.annotation.ColorInt;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
 
 public final class ResourcesUtils {
     private ResourcesUtils() {}
 
     private static final Object mAccessLock = new Object();
-    private static TypedValue mTmpValue = new TypedValue();
+    private static final TypedValue mTmpValue = new TypedValue();
 
-    public static float getFloat(Resources resources, int resId) {
-        TypedValue outValue = new TypedValue();
-        resources.getValue(resId, outValue, true);
-        return outValue.getFloat();
+    /**
+     * Return an integer associated with a particular resource ID.
+     */
+    @CheckResult
+    public static float getFloat(Resources resources, int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
+        synchronized (mAccessLock) {
+            resources.getValue(id, value, true);
+            if (value.type == TypedValue.TYPE_FLOAT) {
+                return value.getFloat();
+            }
+            throw new Resources.NotFoundException("Resource ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
+        }
     }
 
     private static void getAttrValue(Context context, int attrId, TypedValue value) {
         context.getTheme().resolveAttribute(attrId, value, true);
     }
 
-    public static int getAttrColor(Context context, @AttrRes int attrId) {
-        TypedValue value;
+    /**
+     * Returns a boolean associated with a particular attribute in the Theme.
+     */
+    @CheckResult
+    public static boolean getAttrBoolean(Context context, @AttrRes int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
         synchronized (mAccessLock) {
-            value = mTmpValue;
-            if (value == null) {
-                value = new TypedValue();
-            }
-            getAttrValue(context, attrId, value);
-            if (value.type >= TypedValue.TYPE_FIRST_INT
-                    && value.type <= TypedValue.TYPE_LAST_INT) {
-                mTmpValue = value;
-                return value.data;
-            } else {
-                throw new Resources.NotFoundException(
-                        "Attribute ID #0x" + Integer.toHexString(attrId) + " type #0x"
-                                + Integer.toHexString(value.type) + " is not valid");
-            }
-        }
-    }
-
-    public static boolean getAttrBoolean(Context context, @AttrRes int attrId) {
-        synchronized (mAccessLock) {
-            TypedValue value = mTmpValue;
-            if (value == null) {
-                mTmpValue = value = new TypedValue();
-            }
-            getAttrValue(context, attrId, value);
+            getAttrValue(context, id, value);
             if (value.type >= TypedValue.TYPE_FIRST_INT
                     && value.type <= TypedValue.TYPE_LAST_INT) {
                 return value.data != 0;
             }
-            throw new Resources.NotFoundException(
-                    "Attribute ID #0x" + Integer.toHexString(attrId) + " type #0x"
-                            + Integer.toHexString(value.type) + " is not valid");
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
         }
     }
 
-    public static int getAttrDimensionPixelOffset(Context context, @AttrRes int attrId) {
+    /**
+     * Returns a integer associated with a particular attribute in the Theme.
+     */
+    @CheckResult
+    public static int getAttrInteger(Context context, @AttrRes int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
         synchronized (mAccessLock) {
-            TypedValue value = mTmpValue;
-            if (value == null) {
-                mTmpValue = value = new TypedValue();
+            getAttrValue(context, id, value);
+            if (value.type >= TypedValue.TYPE_FIRST_INT
+                    && value.type <= TypedValue.TYPE_LAST_INT) {
+                return value.data;
             }
-            getAttrValue(context, attrId, value);
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
+        }
+    }
+
+    /**
+     * Returns a float associated with a particular attribute in the Theme.
+     */
+    @CheckResult
+    public static float getAttrFloat(Context context, @AttrRes int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
+        synchronized (mAccessLock) {
+            getAttrValue(context, id, value);
+            if (value.type == TypedValue.TYPE_FLOAT) {
+                return value.getFloat();
+            }
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
+        }
+    }
+
+    /**
+     * Retrieve a dimensional for a particular attribute in the Theme.  Unit
+     * conversions are based on the current {@link DisplayMetrics} associated
+     * with the resources.
+     */
+    @CheckResult
+    public static float getAttrDimension(Context context, @AttrRes int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
+        synchronized (mAccessLock) {
+            getAttrValue(context, id, value);
+            if (value.type == TypedValue.TYPE_DIMENSION) {
+                return TypedValue.complexToDimension(
+                        value.data, context.getResources().getDisplayMetrics());
+            }
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
+        }
+    }
+
+    /**
+     * Retrieve a dimensional for a particular attribute in the Theme for use
+     * as an offset in raw pixels.  This is the same as
+     * {@link #getAttrDimension}, except the returned value is converted to
+     * integer pixels for you.  An offset conversion involves simply
+     * truncating the base value to an integer.
+     */
+    @CheckResult
+    public static int getAttrDimensionPixelOffset(Context context, @AttrRes int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
+        synchronized (mAccessLock) {
+            getAttrValue(context, id, value);
             if (value.type == TypedValue.TYPE_DIMENSION) {
                 return TypedValue.complexToDimensionPixelOffset(
                         value.data, context.getResources().getDisplayMetrics());
             }
-            throw new Resources.NotFoundException(
-                    "Attribute ID #0x" + Integer.toHexString(attrId) + " type #0x"
-                            + Integer.toHexString(value.type) + " is not valid");
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
+        }
+    }
+
+    /**
+     * Retrieve a dimensional for a particular attribute in the Theme for use
+     * as a size in raw pixels.  This is the same as
+     * {@link #getAttrDimension}, except the returned value is converted to
+     * integer pixels for use as a size.  A size conversion involves
+     * rounding the base value, and ensuring that a non-zero base value
+     * is at least one pixel in size.
+     */
+    @CheckResult
+    public static int getAttrDimensionPixelSize(Context context, @AttrRes int id)
+            throws Resources.NotFoundException {
+        TypedValue value = mTmpValue;
+        synchronized (mAccessLock) {
+            getAttrValue(context, id, value);
+            if (value.type == TypedValue.TYPE_DIMENSION) {
+                return TypedValue.complexToDimensionPixelSize(
+                        value.data, context.getResources().getDisplayMetrics());
+            }
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(id)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
+        }
+    }
+
+    /**
+     * Returns a color integer associated with a particular attribute in the Theme
+     */
+    @ColorInt
+    @CheckResult
+    public static int getAttrColor(Context context, @AttrRes int attrId) {
+        TypedValue value = mTmpValue;
+        synchronized (mAccessLock) {
+            getAttrValue(context, attrId, value);
+            if (value.type >= TypedValue.TYPE_FIRST_INT
+                    && value.type <= TypedValue.TYPE_LAST_INT) {
+                return value.data;
+            }
+            throw new Resources.NotFoundException("Attribute ID #0x" + Integer.toHexString(attrId)
+                    + " type #0x" + Integer.toHexString(value.type) + " is not valid");
         }
     }
 }
