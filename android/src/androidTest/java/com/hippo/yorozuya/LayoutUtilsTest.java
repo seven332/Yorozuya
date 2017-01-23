29e0ffd2a94ba8d4e58d1999906a31fb4507611c
seven332 <seven332@163.com>
2017-01-23 19:37:25 +0800

    Add LayoutUtils

---
+++ android/src/androidTest/java/com/hippo/yorozuya/LayoutUtilsTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.yorozuya;
+
+/*
+ * Created by Hippo on 1/23/2017.
+ */
+
+import static com.hippo.yorozuya.Utils.assertEqualsFloat;
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LayoutUtilsTest {
+
+  @Test
+  public void testDpPix() {
+    Context app = InstrumentationRegistry.getTargetContext();
+    float density = app.getResources().getDisplayMetrics().density;
+    assertEquals((int) (10 * density + 0.5f), LayoutUtils.dp2pix(app, 10));
+    assertEquals(0, LayoutUtils.dp2pix(app, 0));
+
+    assertEqualsFloat(10 / density, LayoutUtils.pix2dp(app, 10));
+    assertEqualsFloat(0, LayoutUtils.pix2dp(app, 0));
+  }
+
+  @Test
+  public void testSpPix() {
+    Context app = InstrumentationRegistry.getTargetContext();
+    float scaledDensity = app.getResources().getDisplayMetrics().scaledDensity;
+    assertEquals((int) (10 * scaledDensity + 0.5f), LayoutUtils.sp2pix(app, 10));
+    assertEquals(0, LayoutUtils.sp2pix(app, 0));
+
+    assertEqualsFloat(10 / scaledDensity, LayoutUtils.pix2sp(app, 10));
+    assertEqualsFloat(0, LayoutUtils.pix2sp(app, 0));
+  }
+}
---
+++ android/src/main/java/com/hippo/yorozuya/LayoutUtils.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.yorozuya;
+
+/*
+ * Created by Hippo on 1/23/2017.
+ */
+
+import android.content.Context;
+
+public final class LayoutUtils {
+  private LayoutUtils() {}
+
+  /**
+   * Converts dp value to pix value.
+   *
+   * @param context the context
+   * @param dp the dp value to convert
+   * @return the pix value
+   */
+  public static int dp2pix(Context context, float dp) {
+    return (int) (dp * context.getResources().getDisplayMetrics().density + 0.5f);
+  }
+
+  /**
+   * Converts pix value to dp value.
+   *
+   * @param context the context
+   * @param pix the pix value to convert
+   * @return the dp value
+   */
+  public static float pix2dp(Context context, int pix) {
+    return pix / context.getResources().getDisplayMetrics().density;
+  }
+
+  /**
+   * Converts sp value to pix value.
+   *
+   * @param context the context
+   * @param sp the sp value to convert
+   * @return the pix value
+   */
+  public static int sp2pix(Context context, float sp) {
+    return (int) (sp * context.getResources().getDisplayMetrics().scaledDensity + 0.5f);
+  }
+
+  /**
+   * Converts pix value to sp value.
+   *
+   * @param context the context
+   * @param pix the pix value to convert
+   * @return the sp value
+   */
+  public static float pix2sp(Context context, float pix) {
+    return pix / context.getResources().getDisplayMetrics().scaledDensity;
+  }
+}
