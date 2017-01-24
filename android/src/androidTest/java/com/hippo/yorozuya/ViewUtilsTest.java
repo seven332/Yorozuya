03ca1848cd1b154f99438d594b54bc6126c4a004
seven332 <seven332@163.com>
2017-01-24 15:18:35 +0800

    Add ViewUtils

---
+++ android/src/androidTest/java/com/hippo/yorozuya/ViewUtilsTest.java
@@ -0,0 +1,320 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.yorozuya;
+
+/*
+ * Created by Hippo on 1/24/2017.
+ */
+
+import static com.hippo.yorozuya.Utils.assertEqualsFloat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.hippo.yorozuya.android.test.R;
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ViewUtilsTest {
+
+  private Context app;
+
+  @Before
+  public void before() {
+    app = InstrumentationRegistry.getTargetContext();
+  }
+
+  @Test
+  public void testMaxSize() {
+    assertEquals(0x3fffffff, ViewUtils.MAX_SIZE);
+  }
+
+  @Test
+  public void testGetAncestor() {
+    LayoutInflater inflater = LayoutInflater.from(app);
+    View view_1_1 = inflater.inflate(R.layout.test, null);
+
+    assertEquals(view_1_1, ViewUtils.getAncestor(view_1_1, R.id.view_1_1));
+
+    View view_3_3 = view_1_1.findViewById(R.id.view_3_3);
+    assertEquals(view_1_1, ViewUtils.getAncestor(view_3_3, R.id.view_1_1));
+
+    View view_2_1 = view_1_1.findViewById(R.id.view_2_1);
+    assertEquals(view_1_1, ViewUtils.getAncestor(view_2_1, R.id.view_1_1));
+    assertEquals(null, ViewUtils.getAncestor(view_2_1, R.id.view_3_3));
+
+    assertEquals(null, ViewUtils.getAncestor(null, R.id.view_1_1));
+  }
+
+  @Test
+  public void testGetLocationInAncestor() {
+    int[] location = new int[2];
+    LayoutInflater inflater = LayoutInflater.from(app);
+    View view_1_1 = inflater.inflate(R.layout.test, null);
+    view_1_1.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+    view_1_1.layout(0, 0, view_1_1.getMeasuredWidth(), view_1_1.getMeasuredHeight());
+
+    View view_3_4 = view_1_1.findViewById(R.id.view_3_4);
+    Arrays.fill(location, 0);
+    ViewUtils.getLocationInAncestor(view_3_4, R.id.view_1_1, location);
+    assertEquals(50, location[0]);
+    assertEquals(50, location[1]);
+    Arrays.fill(location, 0);
+    ViewUtils.getLocationInAncestor(view_3_4, view_1_1, location);
+    assertEquals(50, location[0]);
+    assertEquals(50, location[1]);
+
+    View view_2_2 = view_1_1.findViewById(R.id.view_2_2);
+    Arrays.fill(location, 0);
+    ViewUtils.getLocationInAncestor(view_3_4, R.id.view_2_2, location);
+    assertEquals(50, location[0]);
+    assertEquals(0, location[1]);
+    Arrays.fill(location, 0);
+    ViewUtils.getLocationInAncestor(view_3_4, view_2_2, location);
+    assertEquals(50, location[0]);
+    assertEquals(0, location[1]);
+
+    view_3_4.setTranslationX(100);
+    view_3_4.setTranslationY(-50);
+    Arrays.fill(location, 0);
+    ViewUtils.getLocationInAncestor(view_3_4, R.id.view_1_1, location);
+    assertEquals(150, location[0]);
+    assertEquals(0, location[1]);
+    view_3_4.setTranslationX(0);
+    view_3_4.setTranslationY(0);
+
+    try {
+      ViewUtils.getLocationInAncestor(null, R.id.view_1_1, location);
+      fail();
+    } catch (NullPointerException e) {
+    }
+    try {
+      ViewUtils.getLocationInAncestor(view_3_4, R.id.view_1_1, null);
+      fail();
+    } catch (IllegalArgumentException e) {
+    }
+    try {
+      ViewUtils.getLocationInAncestor(view_3_4, R.id.view_1_1, new int[1]);
+      fail();
+    } catch (IllegalArgumentException e) {
+    }
+  }
+
+  @Test
+  public void testMeasure() {
+    View view1 = new View(app);
+
+    ViewUtils.measure(view1, 100, 200);
+    assertEquals(100, view1.getMeasuredWidth());
+    assertEquals(200, view1.getMeasuredHeight());
+
+    ViewUtils.measure(view1, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    assertEquals(0, view1.getMeasuredWidth());
+    assertEquals(0, view1.getMeasuredHeight());
+
+    ViewUtils.measure(null, 100, 200);
+  }
+
+  @Test
+  public void testLayout() {
+    View view1 = new View(app);
+
+    ViewUtils.layout(view1, 100, 200);
+    assertEquals(100, view1.getWidth());
+    assertEquals(200, view1.getHeight());
+
+    ViewUtils.layout(view1, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    assertEquals(0, view1.getWidth());
+    assertEquals(0, view1.getHeight());
+
+    ViewUtils.layout(null, 100, 200);
+  }
+
+  @Test
+  public void testGetSuitableSize() {
+    int size;
+    int spec;
+
+    size = 100;
+    spec = View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.EXACTLY);
+    assertEquals(200, ViewUtils.getSuitableSize(size, spec));
+
+    size = 100;
+    spec = View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.UNSPECIFIED);
+    assertEquals(100, ViewUtils.getSuitableSize(size, spec));
+
+    size = 100;
+    spec = View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.AT_MOST);
+    assertEquals(100, ViewUtils.getSuitableSize(size, spec));
+
+    size = 0;
+    spec = View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.AT_MOST);
+    assertEquals(200, ViewUtils.getSuitableSize(size, spec));
+
+    size = 300;
+    spec = View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.AT_MOST);
+    assertEquals(200, ViewUtils.getSuitableSize(size, spec));
+
+    size = 35124;
+    spec = View.MeasureSpec.makeMeasureSpec(200, 213);
+    assertEquals(35124, ViewUtils.getSuitableSize(size, spec));
+  }
+
+  @Test
+  public void testRemoveFromParent() {
+    LayoutInflater inflater = LayoutInflater.from(app);
+    ViewGroup view_1_1 = (ViewGroup) inflater.inflate(R.layout.test, null);
+    View view_2_1 = view_1_1.findViewById(R.id.view_2_1);
+
+    assertEquals(2, view_1_1.getChildCount());
+    ViewUtils.removeFromParent(view_2_1);
+    assertEquals(1, view_1_1.getChildCount());
+
+    ViewUtils.removeFromParent(null);
+    ViewUtils.removeFromParent(view_1_1);
+  }
+
+  @Test
+  public void testIndexInParent() {
+    LayoutInflater inflater = LayoutInflater.from(app);
+    ViewGroup view_1_1 = (ViewGroup) inflater.inflate(R.layout.test, null);
+    View view_2_2 = view_1_1.findViewById(R.id.view_2_2);
+
+    assertEquals(-1, ViewUtils.indexInParent(view_1_1));
+    assertEquals(1, ViewUtils.indexInParent(view_2_2));
+    assertEquals(-1, ViewUtils.indexInParent(null));
+  }
+
+  @Test
+  public void testScreenshot() {
+    TextView view1 = new TextView(app);
+    view1.setText("12345");
+
+    ViewUtils.layout(view1, 100, 200);
+    Bitmap bitmap = ViewUtils.screenshot(view1);
+    assertNotNull(bitmap);
+    assertEquals(100, bitmap.getWidth());
+    assertEquals(200, bitmap.getHeight());
+    bitmap.recycle();
+
+    ViewUtils.layout(view1, 1000000, 1000000);
+    try {
+      ViewUtils.screenshot(view1);
+      fail();
+    } catch (OutOfMemoryError e) {
+    } catch (IllegalArgumentException e) {
+    }
+
+    assertNull(ViewUtils.screenshot(null));
+  }
+
+  @Test
+  public void testTransformPointToViewLocal() {
+    LayoutInflater inflater = LayoutInflater.from(app);
+    View view_1_1 = inflater.inflate(R.layout.test, null);
+    View view_2_2 = view_1_1.findViewById(R.id.view_2_2);
+    ViewUtils.layout(view_1_1, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    float[] point = new float[2];
+
+    point[0] = 10.0f;
+    point[1] = 20.0f;
+    ViewUtils.transformPointToViewLocal(point, view_1_1, view_2_2);
+    assertEqualsFloat(10.0f, point[0]);
+    assertEqualsFloat(-30.0f, point[1]);
+
+    point[0] = 10.0f;
+    point[1] = 20.0f;
+    view_2_2.setTranslationX(10.0f);
+    view_2_2.setTranslationY(-20.0f);
+    ViewUtils.transformPointToViewLocal(point, view_1_1, view_2_2);
+    assertEqualsFloat(0.0f, point[0]);
+    assertEqualsFloat(-10.0f, point[1]);
+
+    try {
+      ViewUtils.transformPointToViewLocal(null, view_1_1, view_2_2);
+      fail();
+    } catch (IllegalArgumentException e) {
+    }
+    try {
+      ViewUtils.transformPointToViewLocal(new float[1], view_1_1, view_2_2);
+      fail();
+    } catch (IllegalArgumentException e) {
+    }
+    try {
+      ViewUtils.transformPointToViewLocal(point, null, view_2_2);
+      fail();
+    } catch (NullPointerException e) {
+    }
+    try {
+      ViewUtils.transformPointToViewLocal(point, view_1_1, null);
+      fail();
+    } catch (NullPointerException e) {
+    }
+    try {
+      ViewUtils.transformPointToViewLocal(point, null, null);
+      fail();
+    } catch (NullPointerException e) {
+    }
+  }
+
+  @Test
+  public void testPointInView() {
+    LayoutInflater inflater = LayoutInflater.from(app);
+    View view_1_1 = inflater.inflate(R.layout.test, null);
+    View view_2_2 = view_1_1.findViewById(R.id.view_2_2);
+    ViewUtils.layout(view_1_1, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    assertTrue(ViewUtils.pointInView(view_2_2, 0.0f, 0.0f));
+    assertFalse(ViewUtils.pointInView(view_2_2, 0.0f, 50.0f));
+    assertTrue(ViewUtils.pointInView(view_2_2, 0.0f, 50.0f, 1.0f));
+    assertFalse(ViewUtils.pointInView(null, 0.0f, 0.0f));
+  }
+
+  @Test
+  public void testTranslationXYBy() {
+    LayoutInflater inflater = LayoutInflater.from(app);
+    View view_1_1 = inflater.inflate(R.layout.test, null);
+    View view_2_2 = view_1_1.findViewById(R.id.view_2_2);
+    ViewUtils.layout(view_1_1, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    assertEqualsFloat(0.0f, view_2_2.getX());
+    assertEqualsFloat(50.0f, view_2_2.getY());
+    ViewUtils.translationXBy(view_2_2, 10.0f);
+    assertEqualsFloat(10.0f, view_2_2.getX());
+    ViewUtils.translationXBy(view_2_2, -30.0f);
+    assertEqualsFloat(-20.0f, view_2_2.getX());
+    ViewUtils.translationYBy(view_2_2, 10.0f);
+    assertEqualsFloat(60.0f, view_2_2.getY());
+    ViewUtils.translationYBy(view_2_2, -30.0f);
+    assertEqualsFloat(30.0f, view_2_2.getY());
+  }
+}
---
+++ android/src/main/java/com/hippo/yorozuya/ViewUtils.java
@@ -0,0 +1,419 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.yorozuya;
+
+/*
+ * Created by Hippo on 1/23/2017.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+public final class ViewUtils {
+  private ViewUtils() {}
+
+  private static final String LOG_TAG = ViewUtils.class.getSimpleName();
+
+  /**
+   * Max size allowed for view.
+   */
+  public static final int MAX_SIZE = Integer.MAX_VALUE & ~(0x3 << 30);
+
+  /** Lock object used to protect access to {@link #tmpMatrix}. */
+  private static final Object tmpMatrixLock = new Object();
+  /** Single-item pool used to minimize Matrix allocations. */
+  private static Matrix tmpMatrix = new Matrix();
+
+  /**
+   * Finds a ancestor view with the given id recursively.
+   *
+   * @param view the view to start with
+   * @param id the desired view identifier
+   * @return the desired view or {@code null}
+   */
+  public static View getAncestor(View view, int id) {
+    if (view == null) {
+      Log.w(LOG_TAG, "getAncestor: view == null");
+      return null;
+    }
+
+    if (view.getId() == id) {
+      return view;
+    }
+
+    ViewParent viewParent = view.getParent();
+    while (viewParent instanceof View) {
+      view = (View) viewParent;
+      if (view.getId() == id) {
+        return view;
+      }
+      viewParent = view.getParent();
+    }
+
+    return null;
+  }
+
+  /**
+   * Get view location in its ancestor.
+   * If can't find the ancestor, get its location in root view.
+   *
+   * @param view the view to start with
+   * @param ancestorId the ancestor id
+   * @param location the container of result
+   * @throws IllegalArgumentException if location is not an array of two integers
+   * @throws NullPointerException if {@code view == null}
+   */
+  public static void getLocationInAncestor(View view, int ancestorId, int[] location)
+      throws IllegalArgumentException, NullPointerException {
+    if (view == null) {
+      throw new NullPointerException("view == null");
+    }
+    if (location == null || location.length < 2) {
+      throw new IllegalArgumentException(
+          "location must be an array of two integers");
+    }
+
+    float[] position = new float[2];
+
+    view.getMatrix().mapPoints(position);
+
+    position[0] += view.getLeft();
+    position[1] += view.getTop();
+
+    ViewParent viewParent = view.getParent();
+    while (viewParent instanceof View) {
+      view = (View) viewParent;
+      if (view.getId() == ancestorId) {
+        break;
+      }
+
+      position[0] -= view.getScrollX();
+      position[1] -= view.getScrollY();
+
+      view.getMatrix().mapPoints(position);
+
+      position[0] += view.getLeft();
+      position[1] += view.getTop();
+
+      viewParent = view.getParent();
+    }
+
+    location[0] = (int) (position[0] + 0.5f);
+    location[1] = (int) (position[1] + 0.5f);
+  }
+
+  /**
+   * Get view location in its ancestor.
+   * If can't find the ancestor, get its location in root view.
+   *
+   * @param view the view to start with
+   * @param ancestor the ancestor
+   * @param location the container of result
+   * @throws IllegalArgumentException if location is not an array of two integers
+   * @throws NullPointerException if {@code view == null} or {@code ancestor == null}
+   */
+  public static void getLocationInAncestor(View view, View ancestor, int[] location)
+      throws IllegalArgumentException, NullPointerException {
+    if (ancestor == null) {
+      throw new NullPointerException("ancestor == null");
+    }
+    getLocationInAncestor(view, ancestor.getId(), location);
+  }
+
+  /**
+   * Measures a view.
+   * <p>
+   * It is useful to get its size before layout.
+   * {@link View#getMeasuredWidth()} and {@link View#getMeasuredHeight()}
+   * will return valid values.
+   *
+   * @param v the view to measure
+   * @param width the width size, {@link ViewGroup.LayoutParams#WRAP_CONTENT} or a positive integer
+   * @param height the height size, {@link ViewGroup.LayoutParams#WRAP_CONTENT} or a positive integer
+   */
+  public static void measure(View v, int width, int height) {
+    if (v == null) {
+      Log.w(LOG_TAG, "measure: v == null");
+      return;
+    }
+    int widthMeasureSpec;
+    int heightMeasureSpec;
+    if (width == ViewGroup.LayoutParams.WRAP_CONTENT) {
+      widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+    } else {
+      widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(Math.max(width, 0), View.MeasureSpec.EXACTLY);
+    }
+    if (height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+      heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+    } else {
+      heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(Math.max(height, 0), View.MeasureSpec.EXACTLY);
+    }
+    v.measure(widthMeasureSpec, heightMeasureSpec);
+  }
+
+  /**
+   * Measures and layouts a view.
+   * <p>
+   * {@link View#getWidth()} and {@link View#getHandler()}
+   * will return valid values.
+   *
+   * @param v the view to layout
+   * @param width the width size, {@link ViewGroup.LayoutParams#WRAP_CONTENT} or a positive integer
+   * @param height the height size, {@link ViewGroup.LayoutParams#WRAP_CONTENT} or a positive integer
+   */
+  public static void layout(View v, int width, int height) {
+    if (v == null) {
+      Log.w(LOG_TAG, "layout: v == null");
+      return;
+    }
+    measure(v, width, height);
+    v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+  }
+
+  /**
+   * Utility to return a default size. Uses the supplied size if the
+   * MeasureSpec imposed no constraints. Will get suitable if allowed
+   * by the MeasureSpec.
+   *
+   * @param size Default size for this view
+   * @param spec Constraints imposed by the parent
+   * @return The size this view should be.
+   */
+  public static int getSuitableSize(int size, int spec) {
+    int result = size;
+    int specMode = View.MeasureSpec.getMode(spec);
+    int specSize = View.MeasureSpec.getSize(spec);
+
+    switch (specMode) {
+      case View.MeasureSpec.UNSPECIFIED:
+        result = size;
+        break;
+      case View.MeasureSpec.EXACTLY:
+        result = specSize;
+        break;
+      case View.MeasureSpec.AT_MOST:
+        return size == 0 ? specSize : Math.min(size, specSize);
+      default:
+        Log.d(LOG_TAG, "getSuitableSize: bad spec mode: " + specMode);
+        break;
+    }
+    return result;
+  }
+
+  /**
+   * Remove view from its parent.
+   *
+   * @param v the view to remove
+   */
+  public static void removeFromParent(View v) {
+    if (v == null) {
+      Log.w(LOG_TAG, "removeFromParent: v == null");
+      return;
+    }
+    ViewParent vp = v.getParent();
+    if (vp instanceof ViewGroup) {
+      ((ViewGroup) vp).removeView(v);
+    }
+  }
+
+  /**
+   * Get index of the view in its parent.
+   *
+   * @param v the view
+   * @return the index in its parent, or {@code -1} if not found
+   */
+  public static int indexInParent(View v) {
+    if (v == null) {
+      Log.w(LOG_TAG, "indexInParent: v == null");
+      return -1;
+    }
+
+    ViewParent vp = v.getParent();
+    if (vp == null) {
+      Log.w(LOG_TAG, "indexInParent: orphan");
+      return -1;
+    }
+    if (! (vp instanceof ViewGroup)) {
+      Log.w(LOG_TAG, "indexInParent: parent isn't a ViewGroup");
+      return -1;
+    }
+
+    ViewGroup vg = (ViewGroup) vp;
+    return vg.indexOfChild(v);
+  }
+
+  /**
+   * Returns a bitmap showing a screenshot of the view passed in.
+   * The view must be measured and layout.
+   *
+   * @param v The view to get screenshot
+   * @return The screenshot, or {@code null} if can't get it
+   * @throws OutOfMemoryError if the view is too large
+   * @throws IllegalArgumentException if the view is too large
+   */
+  public static Bitmap screenshot(View v)
+      throws NullPointerException, OutOfMemoryError {
+    if (v == null) {
+      Log.w(LOG_TAG, "screenshot: v == null");
+      return null;
+    }
+    int width = v.getWidth();
+    int height = v.getHeight();
+    if (width <= 0 || height <= 0) {
+      Log.w(LOG_TAG, "screenshot: width <= 0 || height <= 0");
+      return null;
+    }
+
+    Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    canvas.translate(-v.getScrollX(), -v.getScrollY());
+    v.draw(canvas);
+    return bitmap;
+  }
+
+  /**
+   * Returns a Matrix suitable for temporary use. The obtained Matrix
+   * should be released using {@link #releaseTempMatrix(Matrix)}.
+   *
+   * @return a matrix suitable for temporary use
+   */
+  private static Matrix obtainTempMatrix() {
+    Matrix tmpMatrix = null;
+    synchronized (tmpMatrixLock) {
+      if (ViewUtils.tmpMatrix != null) {
+        tmpMatrix = ViewUtils.tmpMatrix;
+        ViewUtils.tmpMatrix = null;
+      }
+    }
+    if (tmpMatrix == null) {
+      return new Matrix();
+    }
+    return tmpMatrix;
+  }
+
+  /**
+   * Returns a Matrix to the pool. After calling this method, the
+   * specified Matrix should no longer be accessed.
+   *
+   * @param value the typed value to return to the pool
+   */
+  private static void releaseTempMatrix(Matrix value) {
+    synchronized (tmpMatrixLock) {
+      if (tmpMatrix == null) {
+        tmpMatrix = value;
+      }
+    }
+  }
+
+  /**
+   * Transforms the specified point to the coordinate space of a child view.
+   *
+   * @param point the point to transform
+   * @param parent the parent view
+   * @param child the child view
+   * @throws IllegalArgumentException if point is not an array of two integers
+   * @throws NullPointerException if {@code parent == null} or {@code child == null}
+   */
+  public static void transformPointToViewLocal(float[] point, View parent, View child)
+      throws IllegalArgumentException, NullPointerException {
+    if (point == null || point.length < 2) {
+      throw new IllegalArgumentException("point must be an array of two integers");
+    }
+    if (parent == null || child == null) {
+      throw new NullPointerException("parent == null || child == null");
+    }
+    point[0] += parent.getScrollX() - child.getLeft();
+    point[1] += parent.getScrollY() - child.getTop();
+
+    final Matrix value = obtainTempMatrix();
+    try {
+      if (child.getMatrix().invert(value)) {
+        value.mapPoints(point);
+      }
+    } finally {
+      releaseTempMatrix(value);
+    }
+  }
+
+  /**
+   * Determines whether the given point, in local coordinates is inside the view.
+   *
+   * @param view the view
+   * @param localX x of the point
+   * @param localY x of the point
+   * @return {@code true} if the point is in the view and view is not {@code null}
+   */
+  public static boolean pointInView(View view, float localX, float localY) {
+    return pointInView(view, localX, localY, 0);
+  }
+
+  /**
+   * Utility method to determine whether the given point, in local coordinates,
+   * is inside the view, where the area of the view is expanded by the slop factor.
+   * This method is called while processing touch-move events to determine if the event
+   * is still within the view.
+   *
+   * @param view the view
+   * @param localX x of the point
+   * @param localY x of the point
+   * @param slop the slop factor
+   * @return {@code true} if the point is in the view and view is not {@code null}
+   */
+  public static boolean pointInView(View view, float localX, float localY, float slop) {
+    if (view == null) {
+      Log.w(LOG_TAG, "pointInView: view == null");
+      return false;
+    }
+    return localX >= -slop
+        && localY >= -slop
+        && localX < (view.getWidth() + slop)
+        && localY < (view.getHeight() + slop);
+  }
+
+  /**
+   * Offset this view translationX.
+   *
+   * @param view the view
+   * @param offset the translationX offset
+   */
+  public static void translationXBy(View view, float offset) {
+    if (view == null) {
+      Log.w(LOG_TAG, "translationXBy: view == null");
+      return;
+    }
+    view.setTranslationX(view.getTranslationX() + offset);
+  }
+
+  /**
+   * Offset this view translationY.
+   *
+   * @param view the view
+   * @param offset the translationY offset
+   */
+  public static void translationYBy(View view, float offset) {
+    if (view == null) {
+      Log.w(LOG_TAG, "translationYBy: view == null");
+      return;
+    }
+    view.setTranslationY(view.getTranslationY() + offset);
+  }
+}
